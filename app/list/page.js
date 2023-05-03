d96914613b28e8cae7c4285cd24d641f573e4f61
2sel <yunwel0610@gmail.com>
2023-05-03 22:43:50 +0900

    routing_2

--- app/list/page.js
+++ app/list/page.js
@@ -1,8 +1,14 @@
-export default function Home() {
+export default function List() {
   let name = "kim";
   return (
     <div>
       <h4 className="title">상품목록</h4>
+      <div className="food">
+        <p>상품1</p>
+      </div>
+      <div className="food">
+        <p>상품2</p>
+      </div>
     </div>
   );
 }
